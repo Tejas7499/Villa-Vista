ed46b6e8d2b2a3850e5ddc3c26b3e9e1c8420c76
Tejas Sambhaji Jadhav <jtejas191@gmail.com>
2025-11-12 18:25:03 +0530

    styling part done

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override")
+const ejsMate = require("ejs-mate");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
 main()
   .then(() => console.log("connection successful"))
@@ -26,7 +29,7 @@ app.listen(8080, () => {
 });
 
 app.get("/", (req, res) => {
-  res.redirect("/listings")
+  res.send("this is root")
 })
 
 //Index Route
